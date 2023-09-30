11acbc7f8a9e9435978991ac3d1a6da23a7f65d5
Ethan C <72404198+ejco1@users.noreply.github.com>
2023-09-30 09:18:55 -0700

    Basic Script

---
+++ discordgm.py
@@ -0,0 +1,19 @@
+import requests
+import random
+import json
+
+with open('config.json', 'r') as config:
+    config_data = json.load(config)
+
+urls = config_data["urls"]
+auth = {'authorization': config_data["authKey"]}
+variations = config_data["variations"]
+lengthOfArray = len(variations) - 1
+
+def main():
+    for url in urls:
+        requests.post(url, headers = auth, data = {'content': variations[random.randint(0,lengthOfArray)]})
+
+if __name__ == "__main__":
+    main()
+

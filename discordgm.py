be2b7a82b1fd06f40b3e0e8590e862ef36c185c8
Ethan C <72404198+ejco1@users.noreply.github.com>
2023-10-01 17:52:37 -0700

    Added the chat_gpt question version.

--- discordgm.py
+++ discordgm.py
@@ -1,18 +1,29 @@
 import requests
 import random
 import json
+import openai
 
 with open(r"config.json", 'r') as config:
     config_data = json.load(config)
 
 urls = config_data["urls"]
 auth = {'authorization': config_data["authKey"]}
-variations = config_data["variations"]
-lengthOfArray = len(variations) - 1
+openai.api_key = config_data["api_key"]
+question = config_data["question"]
+
+def ask_chatgpt(question):
+    response = openai.Completion.create(
+        engine="text-davinci-002",  # Choose the appropriate GPT-3.5 engine
+        prompt=question,
+        max_tokens=50,  # Adjust max tokens as needed for your response length
+        temperature=0.7,  # Adjust temperature for response randomness
+    )
+    
+    return response.choices[0].text.strip()
 
 def main():
     for url in urls:
-        requests.post(url, headers = auth, data = {'content': variations[random.randint(0,lengthOfArray)]})
+        requests.post(url, headers = auth, data = {'content': ask_chatgpt(question)})
 
 if __name__ == "__main__":
     main()
